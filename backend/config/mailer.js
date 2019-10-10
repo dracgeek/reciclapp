7e26590bfd32eddd624a1d3374f8a7d8a6969c60
johnvalle6 <johnvalle829@hotmail.com>
2019-10-10 14:59:07 -0500

    confgemailsend

---
+++ backend/config/mailer.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MAILGUN_USER:
+    "postmaster@sandboxdf4fa64f2da44426a73495e02725a120.mailgun.org",
+  MAILGUN_PASS: "d0810b9127c86cbc2c6cc9fdf8f72067-9949a98f-638f82a1"
+};
---
+++ backend/config/passport.js
@@ -0,0 +1,53 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+passport.use(
+  "local",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: false
+    },
+    async (email, password, done) => {
+      try {
+        // 1) Check if the email already exists
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          return done(null, false, { message: "Unknown User" });
+        }
+
+        // 2) Check if the password is correct
+        const isValid = User.comparePasswords(password, user.password);
+        if (!isValid) {
+          return done(null, false, { message: "Unknown Password" });
+        }
+
+        // 3) Check if email has been verified
+        if (!user.active) {
+          return done(null, false, {
+            message: "Sorry, you must validate email first"
+          });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
+      }
+    }
+  )
+);
