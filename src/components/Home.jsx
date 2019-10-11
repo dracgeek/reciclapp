fd14d542c8c7dc31ae1a910c5cb89f0b0648d4ac
orlyn rosales <orlynroasales1920@gmail.com>
2019-10-10 21:44:25 -0500

    Login
    Se agregaron los cambios de estilos e input para el login de los usuarios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../styles/styleHome.css'
 
 
 class Home extends React.Component {
@@ -10,7 +11,19 @@ class Home extends React.Component {
     render () {
         return (
             <React.Fragment>
-               
+               <div className="container-fluid">
+                    <div className="row ">
+                        <div className="col mt-5 d-flex align-items-center justify-content-center flex-column">
+                            <img src="../../document/logo.jpg" alt="" className="logo mb-3" />
+                           <Link to="/login">
+                                <button type="submit" className="btn btn-lg btn-login mb-4">INICIAR SESION</button>
+                           </Link>
+                           <Link to="/RegisterOne">
+                                <button type="submit" className="btn btn-lg btn-signup">REGISTRARSE</button>
+                           </Link>
+                        </div>
+                    </div>
+               </div>
             </React.Fragment>
         )
     }
