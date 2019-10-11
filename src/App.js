0822c0d8c0869679f1995a57c7d1bd8126279859
orlyn rosales <orlynroasales1920@gmail.com>
2019-10-10 21:58:16 -0500

    App
    Componentes Agregados

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from './components/Home';
 import Login from './components/Login';
+import RegisterOne from './components/RegisterOne';
+import RegisterForm from './components/RegisterForm';
 
 
 function App() {
   return (
     <BrowserRouter>
          <Switch>
+
          <Route exact path="/" component={Home} />   
          <Route exact path="/login" component={Login} />   
+         <Route exact path="/registerone" component={RegisterOne}/>
+         <Route exact path="/registerform" component={RegisterForm}/>
          </Switch>
    
 
