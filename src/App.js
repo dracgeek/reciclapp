0ba8c492bcce0ecff435ccc3f6a2c7533352745e
orlyn rosales <orlynroasales1920@gmail.com>
2019-10-13 18:24:01 -0500

    Agregando nuevas componentes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Home from './components/Home';
 import Login from './components/Login';
 import RegisterOne from './components/RegisterOne';
 import RegisterForm from './components/RegisterForm';
-
+import Confirmation from './components/Confirmation';
+import Instructive from './components/Instructive';
+import MaterialScanner from './components/MaterialScanner';
 
 function App() {
   return (
@@ -15,6 +17,10 @@ function App() {
          <Route exact path="/login" component={Login} />   
          <Route exact path="/registerone" component={RegisterOne}/>
          <Route exact path="/registerform" component={RegisterForm}/>
+         <Route exact path="/confirmation" component={Confirmation}/>
+         <Route exact path="/instructive" component={Instructive}/>
+         <Route exact path="/MaterialScanner" component={MaterialScanner}/>
+
          </Switch>
    
 
