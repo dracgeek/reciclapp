888611b61e28adadd3461a63f40f8a792a8d0e8f
superyaz <yacamocursos@hotmail.com>
2019-10-17 01:45:42 -0500

    Se corrige el error que se generaba al momento de escaner un codigo de barras, se esta recibiendo los datos en un array de objetos por consola

--- src/components/MaterialScanner.jsx
+++ src/components/MaterialScanner.jsx
@@ -1,78 +1,98 @@
 import React from "react";
-import QrReader from "react-qr-scanner";
+// import QrReader from "react-qr-scanner";
 import { Link } from "react-router-dom";
-import Scanner from "../components/Scanner"
-import '../styles/styleMaterialScanner.css';
-
+// import Scanner from "../components/Scanner"
+import "../styles/styleMaterialScanner.css";
 
 class MaterialScanner extends React.Component {
-    
-  constructor(props){
+  constructor(props) {
     super(props);
-    this._onBarcodeDetect = this._onBarcodeDetect.bind(this)
+    this._onBarcodeDetect = this._onBarcodeDetect.bind(this);
   }
 
-  _onBarcodeDetect(barcode){
-    this.props.process(barcode)
+  _onBarcodeDetect(barcode) {
+    this.props.process(barcode);
   }
-    render() {
-        return (
-          <React.Fragment>
-             <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <div className="row">
-                            <div className="col-12 col-md-6">
-                                <Link to='/registerform'>
-                                    <img src="../../document/logo.jpg" className="logo" alt=""/>
-                                </Link>  
-                            </div>
-                            
-                            <div className="col-12 d-flex align-items-center justify-content-center">
-                                <h3>Material a Reciclar</h3>
-                            </div>
-                        </div>
-                        <form action="">
-                            <div className="form-group row">
-                                <div className="col col-12">
-                                    <h6>Codigo de Barra</h6>
-                                    <input type="number" placeholder="codigo de barra" className="form-control"/>
-                                </div>
-                            </div>
-                            <div className="form-group row">
-                                <div className="col col-12">
-                                    <h6>Tipo de Material</h6>
-                                    <select name="text" id="" className="form-control">
-                                        <option value="">Carton</option>
-                                        <option value="">Plastico</option>
-
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="form-group row">
-                                <div className="col col-12 ">
-                                    <h6>Cantidad de Material</h6>
-                                    <input type="number" id="" className="form-control" min="0" max="1000"/>
-                                </div>
-                            </div>
-                            <div className="form-group row d-flex mb-4 justify-content-center">
-                                <Link to='/Scanner'> 
-                                    <button type="button" className="form-control btn btn-lg btn-escanear" name="Escanear Nuevamente">Escanear Nuevamente</button>
-                                </Link>
-                                <Link to='/tostore'> 
-                                    <button type="button" className="form-control btn btn-lg btn-almacenar" name="Almacenar Nuevamente">Almacenar</button>
-                                </Link>
-                            </div>
-                        </form>
-                    </div>
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="row">
+                <div className="col-12 col-md-6">
+                  <Link to="/registerform">
+                    <img
+                      src="../../document/logo.jpg"
+                      className="logo"
+                      alt=""
+                    />
+                  </Link>
                 </div>
-             </div>  
-          </React.Fragment>
-        )
-    }
- 
+
+                <div className="col-12 d-flex align-items-center justify-content-center">
+                  <h3>Material a Reciclar</h3>
+                </div>
+              </div>
+              <form action="">
+                <div className="form-group row">
+                  <div className="col col-12">
+                    <h6>Codigo de Barra</h6>
+                    <input
+                      type="number"
+                      placeholder="codigo de barra"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <div className="col col-12">
+                    <h6>Tipo de Material</h6>
+                    <select name="text" id="" className="form-control">
+                      <option value="">Carton</option>
+                      <option value="">Plastico</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <div className="col col-12 ">
+                    <h6>Cantidad de Material</h6>
+                    <input
+                      type="number"
+                      id=""
+                      className="form-control"
+                      min="0"
+                      max="1000"
+                    />
+                  </div>
+                </div>
+                <div className="form-group row d-flex mb-4 justify-content-center">
+                  <Link to="/Scanner">
+                    <button
+                      type="button"
+                      className="form-control btn btn-lg btn-escanear"
+                      name="Escanear Nuevamente"
+                    >
+                      Escanear Nuevamente
+                    </button>
+                  </Link>
+                  <Link to="/tostore">
+                    <button
+                      type="button"
+                      className="form-control btn btn-lg btn-almacenar"
+                      name="Almacenar Nuevamente"
+                    >
+                      Almacenar
+                    </button>
+                  </Link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-
-
-export default MaterialScanner
+export default MaterialScanner;
--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,63 +1,65 @@
-import React,{ Component,Fragment} from "react";
-import QrReader from "react-qr-scanner"
-import '../styles/styleScanner.css';
-import Quagga from 'quagga';
+import React, { Component, Fragment } from "react";
+// import QrReader from "react-qr-scanner";
+import "../styles/styleScanner.css";
+import Quagga from "quagga";
 
-
-class Scanner extends Component  {
-
-  constructor(props){
+class Scanner extends Component {
+  constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       nocamera: false
-    }
-    this.onDetect = this.onDetect.bind(this)
-    
+    };
+    // this.onDetect = this.onDetect.bind(this);
   }
 
-  
-  componentDidMount(){
-    Quagga.init({
-      inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        constraints: {
-          width: '790',
-          height: '490'
+  componentDidMount() {
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          constraints: {
+            width: "790",
+            height: "490"
+          },
+          numberOfWorkers: navigator.hardwareConcurrency,
+          target: document.querySelector("#barcodeScan")
         },
-        numberOfWorkers: navigator.hardwareConcurrency,
-        target: document.querySelector('#barcodeScan')
+        locate: true,
+        decoder: {
+          readers: ["code_128_reader", "upc_reader", "upc_e_reader"]
+        }
       },
-      locate: true,
-      decoder: {
-        readers: ["code_128_reader", "upc_reader", "upc_e_reader"]
+      function(err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        Quagga.start();
       }
-    }, function (err) {
-      if (err) {
-        return 
-      }
-      Quagga.start()
-    })
-    Quagga.onDetected(this.onDetect)
-    
+    );
+    Quagga.onDetected(data => {
+      console.log(data);
+    });
   }
 
-  onDetect(res){
-    // console.log(res.codeResult.code)
-    Quagga.stop()
-    Quagga.offProcessed()
-    this.props.onBarcodeDetect(res.codeResult.code)
-  }
+  // onDetect(data, res) {
+  //   console.log(data);
+  //   Quagga.stop();
+  //   Quagga.offProcessed();
+  //   this.props.onBarcodeDetect(res.codeResult.code);
+  // }
 
   render() {
     return (
       <Fragment>
-            <div d-flex justify-content-center><h4>ESCANER</h4></div>
-            <div id="barcodeScan"></div>
+        <div d-flex justify-content-center>
+          <h4>ESCANER</h4>
+        </div>
+        <div id="barcodeScan"></div>
       </Fragment>
-    )
+    );
   }
-} 
-  
-export default Scanner;
+}
 
+export default Scanner;
